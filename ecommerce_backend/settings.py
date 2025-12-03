b1e03409e6ab923a929cf0fc76655d3d71357739
bahyani <ikennaiwu@yahoo.com>
2025-12-03 23:58:31 +0100

    E-Commerce

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -98,16 +98,22 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600  # Optional: keep connections open for better performance
+    )
 
 
 # Password validation
